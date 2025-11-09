786ffb8cd5f07f14cc7ba220018977ba02e0f9cd
leonDenizard <de.nizard@hotmail.com>
2025-11-09 19:16:55 -0300

    feat(auth): add bcrypt password hashing in pre-save hook

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,5 +1,7 @@
-import { timeStamp } from "console"
 import { Schema, model, Document } from "mongoose"
+import bcrypt from "bcrypt"
+
+const SALT_ROUNDS = 12
 
 //Criando Interface
 export interface IUser extends Document {
@@ -21,5 +23,20 @@ const userSchema = new Schema<IUser>(
         timestamps: true,
     },
 )
+
+//Pre-save: hash quando cria ou quando senha é alterada
+userSchema.pre("save", async function (next){
+    const doc = this as IUser
+    if(!doc.isModified("password")) return next()
+
+    try {
+        const hash = await bcrypt.hash(doc.password, SALT_ROUNDS)
+        doc.password = hash
+
+        next()
+    } catch (error) {
+        next(error as any)
+    }
+})
 
 export const User = model<IUser>("User", userSchema)
