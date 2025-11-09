ce284da23fcc2e1828275c8abbfe8ef14c322e86
leonDenizard <de.nizard@hotmail.com>
2025-11-09 16:54:42 -0300

    feat(user): add User model for MongoDb data storage

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -12,10 +12,11 @@ export interface IUser extends Document {
 //Schema mongoose define o formato no BD
 const userSchema = new Schema<IUser>(
 
-    {username: { type: String, required: true, unique: true},
-    email: { type: String, required: true, unique: true},
-    password: { type: String, required: true},
-},
+    {
+        username: { type: String, required: true, unique: true },
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+    },
     {
         timestamps: true,
     },
